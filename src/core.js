87d22c813affa7abf8f6d25a9514776e92475b96
mlync87 <martylynch35@hotmail.com>
2022-09-19 11:51:00 +0100

    functions 2 edited

--- src/core.js
+++ src/core.js
@@ -1,24 +1,33 @@
 // 1. Create a function named bakingTime that returns 50
-
+function bakingTime() {
+  return 50
+}
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
 //
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
-
+function remainingBakeTime(numberMinsCakeInOven) {
+  return bakingTime() - numberMinsCakeInOven
+}
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
 //
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
-
+function calculatePreparationTime(numLayers) {
+  return numLayers * 3
+}
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
 // - the number of minutes the cake has already been baking in the oven
 // It must return how many minutes in total you have spent making the cake,
 // which is the sum of the preparation time and the number of minutes it's been in the oven.
 // Use your calculatePreparationTime function in the calculation.
-
+function totalTimeSpent(numLayers, numMinsCakeAlreadyInOven) {
+  const prepTime = calculatePreparationTime(numLayers)
+  return prepTime + numMinsCakeAlreadyInOven
+}
 // Don't change the code below this line
 module.exports = {
   /* eslint-disable no-undef */
